c233c60ccf1b43d5d3dfcbbaa846e5837c95958d
douglasslaviero <emaildodouglasslaviero@gmail.com>
2021-06-22 22:12:37 -0300

    Added login process with spotify auth sdk, and added the client ID to the local.properties instead of being in core code

--- app/src/main/java/app/spotravel/MainActivity.java
+++ app/src/main/java/app/spotravel/MainActivity.java
@@ -2,13 +2,63 @@ package app.spotravel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.spotify.sdk.android.auth.AuthorizationClient;
+import com.spotify.sdk.android.auth.AuthorizationRequest;
+import com.spotify.sdk.android.auth.AuthorizationResponse;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1337;
+    private static final String REDIRECT_URI = "app.spotravel://callback";
+    private String CLIENT_ID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CLIENT_ID = getResources().getString(R.string.spotify_client_id);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthorizationResponse response = AuthorizationClient.getResponse(resultCode, intent);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    Log.d("Logado?", "Sim meu bom");
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    Log.d("Logado?", "Não :(");
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
+    }
+
+    public void login(View view) {
+        AuthorizationRequest.Builder builder =
+                new AuthorizationRequest.Builder(CLIENT_ID, AuthorizationResponse.Type.TOKEN, REDIRECT_URI);
+
+        builder.setScopes(new String[]{"user-read-recently-played"});
+        AuthorizationRequest request = builder.build();
+
+        AuthorizationClient.openLoginActivity(this, REQUEST_CODE, request);
     }
 }
